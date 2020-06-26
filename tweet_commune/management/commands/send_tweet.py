055d9bdca93f191a8d2924025dd7d620a7d84b4c
Jack Phallen <jackphallen@gmail.com>
2020-06-26 10:15:13 -0400

    Include logging throughout send_tweet process.
    Call TweetLogger throughout the send_tweet management command process
    to log status of the cron task

--- tweet_commune/management/commands/send_tweet.py
+++ tweet_commune/management/commands/send_tweet.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from ...models.submission import Submission
 from tweet_commune.submission_actions import SubmissionActions
+from ...logger.tweet_logger import TweetLogger
 
 
 class Command(BaseCommand):
@@ -11,7 +12,12 @@ class Command(BaseCommand):
     help = "Post the top submission"
 
     def handle(self, *args, **options):
-        submission = Submission.queue.top()
-        if submission:
-            actions = SubmissionActions(submission)
-            actions.post()
+        try:
+            submission = Submission.queue.top()
+            if submission:
+                actions = SubmissionActions(submission)
+                actions.post()
+            else:
+                TweetLogger.empty_queue()
+        except Exception as e:
+            TweetLogger.critical(e)
--- tweet_commune/submission_actions.py
+++ tweet_commune/submission_actions.py
@@ -1,6 +1,7 @@
 from tweet_commune.tweet_blaster.email_config import EmailConfig
 from tweet_commune.tweet_blaster.email_tweeter import EmailTweeter
 from tweet_commune.tweet_blaster import _CONFIG_PATH, _PROFILE
+from .logger.tweet_logger import TweetLogger
 
 
 class SubmissionActions:
@@ -15,8 +16,13 @@ class SubmissionActions:
         """
         Posts a submission to Twitter
         """
-        config = EmailConfig(_CONFIG_PATH, _PROFILE)
-        tweeter = EmailTweeter(config)
-        tweeter.send_tweet(self.submission.text)
-        self.submission.sent = True
-        self.submission.save()
+        try:
+
+            config = EmailConfig(_CONFIG_PATH, _PROFILE)
+            tweeter = EmailTweeter(config)
+            tweeter.send_tweet(self.submission.text)
+            self.submission.sent = True
+            self.submission.save()
+            TweetLogger.success(self.submission.text)
+        except Exception as e:
+            TweetLogger.tweet_failed(e)
