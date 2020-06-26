1584ab264ae3a8010cab0b3952c4df99a6a4a5e3
Jack Phallen <jackphallen@gmail.com>
2020-06-26 10:24:18 -0400

    Create urls for log views
    Add paths to urls.py pointing to LogListView and LogDetailVew

--- tweet_commune/urls.py
+++ tweet_commune/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import submission, home
+from .views import submission, home, log
 
 urlpatterns = [
-    path('', home.HomeView.as_view(), name="submission_list"),
+    path('', home.HomeView.as_view(), name="home"),
     path('new/', submission.SubmissionCreateView.as_view(), name="submission_new"),
+    path('log/', log.LogListView.as_view(), name="logentry_list"),
+    path('log/<slug:pk>', log.LogDetailView.as_view(), name="logentry_detail"),
 ]
