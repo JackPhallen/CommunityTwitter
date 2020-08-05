64cda5b816fec82c384267270a714dee0c320b5a
Jack Phallen <jackphallen@gmail.com>
2020-08-05 18:12:33 -0400

    Remove email based Twitter API
    Remove tweet_blaster package as it has been replaced by the official
    Twitter API implemented by twitter_api.py

--- tweet_commune/tweet_blaster/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-_DIR = os.path.dirname(__file__)
-_CONFIG_FILE = 'config.ini'
-_CONFIG_PATH = os.path.join(_DIR, _CONFIG_FILE)
-_PROFILE = 'commune_config'
-_IMG_PROFILE = 'commune_img'
