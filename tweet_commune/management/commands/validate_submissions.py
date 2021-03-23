d6fc3aed9c8ebc624b2724d3e07b197556e79132
Jack Phallen <jackphallen@gmail.com>
2021-03-23 18:09:57 -0400

    Stop filter from double flagging submissions
    Currently, submissions will be flagged each time the filter job runs
    and each time a submission is posted. This fix no longer checks
    submissions before posting and allows submissions to be filtered by
    date created.

--- tweet_commune/management/commands/validate_submissions.py
+++ tweet_commune/management/commands/validate_submissions.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.core.management.base import BaseCommand
 
 from tweet_commune.submission_actions import SubmissionActions
@@ -13,8 +15,29 @@ class Command(BaseCommand):
 
     help = "Validate all Submissions in queue"
 
+    def add_arguments(self, parser):
+        # Length (in days) of submissions to filter
+        parser.add_argument(
+            '--days',
+            type=int,
+            default=0,
+            help='Filters all submissions created in the past n days.',
+        )
+
+    _ONE_DAY = datetime.timedelta(days=1)
+
     def handle(self, *args, **options):
-        for submission in Submission.queue.all():
+        days = 1
+        if options['days']:
+            days = options['days']
+        # 1 full day is the minimum allowed
+        if days < 1:
+            raise Exception("--days must be greater than 0")
+        # Get all submissions created within the past n days
+        from_date = datetime.date.today() - datetime.timedelta(days=options['days'])
+        to_filter = Submission.objects.all().filter(date_created__gte=from_date)
+        # Check each submission against the filter
+        for submission in to_filter:
             action = SubmissionActions(submission)
             if not action.validate():
                 action.flag()
